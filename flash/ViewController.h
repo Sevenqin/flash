3fd01bc12a6e2e35e4bd711b22dee58f27f6b32f
sevenqin <sevenqsh@gmail.com>
2016-09-13 18:31:56 +0800

    Initial Commit

---
+++ flash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flash
+//
+//  Created by seven on 16/9/13.
+//  Copyright © 2016年 seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
